a1c3b4e08c5e300093534e8dbbf7df5d547a7222
robingitpro <118666709+robingitpro@users.noreply.github.com>
2024-06-28 13:17:54 +0530

    Update _get_customers.blade.php

--- resources/views/partials/_get_customers.blade.php
+++ resources/views/partials/_get_customers.blade.php
@@ -7,16 +7,9 @@
 
     <td><a href="{{route('customers.edit',encrypt($customer->id))}}" style="margin-right: 10px;"><i
                 class="bi bi-pencil-square"></i></a>
-        {{-- <a href="javascript:void(0);" onclick="event.preventDefault();
-                                                document.getElementById('delete-form-{{ $customer->id }}').submit();"><i
-                class="bi bi-x-circle"></i></a> --}}
+      
     </td>
 
-    {{--<form method="post" action="{{route('customers.destroy', encrypt($customer->id))}}" style="display:none"
-        id="delete-form-{{$customer->id}}">
-        @csrf
-        @method('DELETE')
-    </form> --}}
 
 </tr>
 @endforeach
